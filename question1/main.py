1d2830bf120b5d44469c9c22583551800eb91035
Thomas Tang <tht@dyn3207-211.wlan.ic.ac.uk>
2019-10-22 15:44:22 +0100

    segmenting data

--- question1/main.py
+++ question1/main.py
@@ -1,1 +1,29 @@
-import numpy
+import scipy.io 
+import numpy as np
+
+mat = scipy.io.loadmat('face.mat')
+
+raw_data = mat['X']
+
+
+raw_data = np.transpose(raw_data)
+
+training_data = np.empty([int(520*0.8), 2576])
+testing_data = np.empty([int(520*0.2), 2576])
+
+print(training_data)
+print(len(training_data))
+
+for x in range(52):
+	
+	training_data[x*8:(x+1)*8] = raw_data[x*10:x*10+8]
+	testing_data[x*2:(x+1)*2] = raw_data[x*10+8:(x+1)*10]
+
+print(len(raw_data))
+print(raw_data)
+print("training_data")
+print(len(training_data))
+print(training_data)
+print("testing_data")
+print(len(testing_data))
+print(testing_data)
