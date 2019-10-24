620a690689115a5f72ca78bdb058be323533a4ed
Thomas Tang <tht@dyn3198-247.wlan.ic.ac.uk>
2019-10-24 13:09:09 +0100

    added timing, + cleanups

--- question1/main.py
+++ question1/main.py
@@ -18,34 +18,26 @@ raw_data = np.transpose(raw_data)
 training_data = np.empty([int(520*0.8), 2576])
 testing_data = np.empty([int(520*0.2), 2576])
 
-print(training_data)
-print(len(training_data))
 
 for x in range(52):
         #8/2 ratio for training and testing datasets	
 	training_data[x*8:(x+1)*8] = raw_data[x*10:x*10+8]
 	testing_data[x*2:(x+1)*2] = raw_data[x*10+8:(x+1)*10]
 
-print(len(raw_data))
-print(raw_data)
-print("training_data")
-print(len(training_data))
-print(training_data)
-print("testing_data")
-print(len(testing_data))
-print(testing_data)
-
 #compute the covariance matrix for low-dimensional comutation
-cov_mat = np.cov(training_data)
+start = time.time()
+low_cov_mat = np.cov(training_data)
+end = time.time()
+print("time required to compute low-dim-cov-mat : ",end - start)
 print("length of covariance matrix")
-print(len(cov_mat))
+print(len(low_cov_mat))
 
 #compute the eigenvalues and eigenvectors of the covariance matrix
-w, v = LA.eig(cov_mat)
+start = time.time()
+low_w, low_v = LA.eig(low_cov_mat)
+end = time.time()
+print("time required to compute eigenvalues and eigenvectors of low-dim-cov-mat : ",end-start)
 
-#compute
 
-print('w')
-print(len(w))
-print('v')
-print(v)
+
+
